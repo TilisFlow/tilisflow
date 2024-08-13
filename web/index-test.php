4d13afb09b862e84732139bef5483aa0822f7776
ChristianHoefer <info@choefer.info>
2024-08-13 12:39:02 +0200

    allow Tests are browsing urls

--- web/index-test.php
+++ web/index-test.php
@@ -1,7 +1,8 @@
 <?php
+$isTest=false;
 
 // NOTE: Make sure this file is not accessible when deployed to production
-if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
+if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || $isTest) {
     die('You are not allowed to access this file.');
 }
 
